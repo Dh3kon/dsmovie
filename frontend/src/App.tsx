cbc3ce360fcfa1a0e60bfc5c2919acede8c8ceb2
Kleber Silva <kb-max@hotmail.com>
2022-05-10 21:18:05 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import Navbar from './components/Navbar'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Main Page</h1>
-    </div>
+   <Navbar />
   );
 }
 
