4f650b47392f4f6a0ec5f115eee696a353342b2a
Kleber Silva <kb-max@hotmail.com>
2022-05-10 21:53:01 -0300

    Form layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Navbar from './components/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Listing />} />
-      <Route path='/form'>
-        <Route path=':movieId' element={<Form />} />
-      </Route>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
