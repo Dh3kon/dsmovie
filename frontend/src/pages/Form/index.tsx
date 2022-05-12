d3c173064c969c1ecbc26e5f78aa7aaf114e6cc0
Kleber Silva <kb-max@hotmail.com>
2022-05-11 21:04:28 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,7 +1,7 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 const Form = () => {
-
   const movie = {
     id: 1,
     image:
@@ -12,32 +12,40 @@ const Form = () => {
   };
 
   return (
-      <div className="dsmovie-form-container">
-          <img src={movie.image} alt={movie.title} className="dsmovie-movie-card-image" />
-          <div className="dsmovie-card-bottom-container">
-              <h3>{movie.title}</h3>
-              <form  className="dsmovie-form">
-                  <div className="form-group dsmovie-form-group">
-                      <label htmlFor="email">Informe seu email</label>
-                      <input type="email" className="form-control" id="email" />
-                  </div>
-                  <div className="form-group dsmovie-form-group">
-                      <label htmlFor="score">Informe sua avaliação</label>
-                      <select  id="score" className="form-control">
-                          <option>1</option>
-                          <option>2</option>
-                          <option>3</option>
-                          <option>4</option>
-                          <option>5</option>
-                      </select>
-                  </div>
-                  <div className="dsmovie-form-btn-container">
-                      <button type="submit" className="btn btn-primary dsmovie-btn mt-3">Salvar</button>
-                  </div>
-              </form>
-              <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+    <div className="dsmovie-form-container">
+      <img
+        src={movie.image}
+        alt={movie.title}
+        className="dsmovie-movie-card-image"
+      />
+      <div className="dsmovie-card-bottom-container">
+        <h3>{movie.title}</h3>
+        <form className="dsmovie-form">
+          <div className="form-group dsmovie-form-group">
+            <label htmlFor="email">Informe seu email</label>
+            <input type="email" className="form-control" id="email" />
           </div>
+          <div className="form-group dsmovie-form-group">
+            <label htmlFor="score">Informe sua avaliação</label>
+            <select id="score" className="form-control">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+          <div className="dsmovie-form-btn-container">
+            <button type="submit" className="btn btn-primary dsmovie-btn mt-3">
+              Salvar
+            </button>
+          </div>
+        </form>
+        <Link to="/">
+          <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        </Link>
       </div>
+    </div>
   );
 };
 export default Form;
