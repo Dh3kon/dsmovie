ab6e97e5a1b03ec3e8ada14cbcaef750278faaa5
Kleber Silva <kb-max@hotmail.com>
2022-05-15 14:52:34 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -20,15 +20,21 @@ const Listing = () => {
   });
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`).then(res => {
-      const data = res.data as MoviePage;
-      setPage(data);
-    });
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+      .then(res => {
+        const data = res.data as MoviePage;
+        setPage(data);
+      });
   }, [pageNumber]);
+
+  const handlePageChange = (newPageNumber: number) => {
+    setPageNumber(newPageNumber);
+  };
 
   return (
     <>
-      <Pagination />
+      <Pagination page={page} onChange={handlePageChange} />
       <div className="container">
         <div className="row">
           {page.content.map(movie => (
